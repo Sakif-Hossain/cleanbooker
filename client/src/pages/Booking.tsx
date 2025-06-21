b2e80ad561cdc7b383057ced24a40024f4441fc9
SheikhSH <sakifhossain71@gmail.com>
2025-06-21 14:21:39 -0500

    task6:Initialize frontend

---
+++ client/src/pages/Booking.tsx
@@ -0,0 +1,11 @@
+export default function Booking() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen text-center">
+      <h1 className="text-4xl font-bold mb-4">Welcome to CleanBooker</h1>
+      <p className="text-lg mb-6">This is booking page!</p>
+      <button className="bg-blue-600 text-white px-6 py-2 rounded hover:bg-blue-700">
+        Book
+      </button>
+    </div>
+  );
+}
---
+++ client/src/pages/SignUp.tsx
@@ -0,0 +1,11 @@
+export default function SignUp() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen text-center">
+      <h1 className="text-4xl font-bold mb-4">Welcome to CleanBooker</h1>
+      <p className="text-lg mb-6">This is signup page.</p>
+      <button className="bg-blue-600 text-white px-6 py-2 rounded hover:bg-blue-700">
+        SignUp
+      </button>
+    </div>
+  );
+}
